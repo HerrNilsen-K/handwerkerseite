622df6acac6fed6a904f386fa94e0155cce7bef5
FlareCO <admin@flareco.net>
2021-09-22 21:30:42 +0200

    yeet

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'].'/.inc/config.php';
+include $_SERVER['DOCUMENT_ROOT'].'/lib/router.php';
+
+
+?>
---
+++ lib/router.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class Router {
+    public $request;
+
+    public $routes = [];
+
+    public function __construct(array $request){
+        $this->request = basename($request['REQUEST_URI']);
+    }
+
+    public function addRoute(string $uri, \Closure $fn) : void{
+        $this->routes[$uri] = $fn;
+    }
+
+    public function hasRoute(string $uri) : bool{
+        return array_key_exists($uri, $this->routes);
+    }
+
+    public function run(){
+        if($this->hasRoute($this->request)) {
+            $this->routes[$this->request]->call($this);
+        }
+    }
+}
+
+$router = new Router($_SERVER);
+
+$router->addRoute('', function() {
+    echo 'meow!';
+});
+
+$router->run();
