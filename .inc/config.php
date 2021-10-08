d9aa8555a9bbba515fa0abf5fcefdf023c02e842
FlareCO <admin@flareco.net>
2021-10-08 09:16:11 +0200

    Update config.php

--- .inc/config.php
+++ .inc/config.php
@@ -6,14 +6,15 @@ $app = array(
     "slogan" => "Wir Nageln auch das kleinste Loch!"
 );
 
-/*
+
+
 try {
     $conn = new PDO("mysql:host=DATABASE_HOST;dbname=DATABASE_NAME", "USERNAME", "PASSWORD");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e){
     die("Connection to the Database could't be established! => ".$e->getMessage());
 }
-*/
+
 
 
 ?>
